6f47c0a28701a2fec453e33f8dbd44407aa97880
Eng-Abdelrahman-Ibrahim <abdelrahman11899@gmail.com>
2025-09-26 07:58:19 +0300

    Added separate environments: dev,test,prod, now jenkins will ask which environemnt you want to run

--- EKS/terraform/eks.tf
+++ EKS/terraform/eks.tf
@@ -8,25 +8,32 @@ resource "aws_launch_template" "myapp_nodes" {
   name_prefix = "myapp-nodes-"
 
   user_data = base64encode(<<-EOT
-              #!/bin/bash
-              set -e
-              # Install EKS-supported containerd
-              dnf install -y yum-utils
-              yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-              dnf install -y containerd.io
+                #!/bin/bash
+                set -e
+                # Install EKS-supported containerd
+                dnf install -y yum-utils
+                yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+                dnf install -y containerd.io
 
-              # Configure containerd for CRI
-              mkdir -p /etc/containerd
-              containerd config default | tee /etc/containerd/config.toml
-              sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+                # Configure containerd for CRI
+                mkdir -p /etc/containerd
+                containerd config default | tee /etc/containerd/config.toml
 
-              systemctl enable containerd
-              systemctl restart containerd
+                # Enable systemd cgroups
+                sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 
-              # Bootstrap EKS
-              /etc/eks/bootstrap.sh ${local.name}
-              EOT
-  )
+                # Add Nexus registry mirror
+                sed -i '/registry.mirrors]/a\\
+  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."${data.kubernetes_service.nexus_docker_lb.status[0].load_balancer[0].ingress[0].hostname}:8082"]\\
+    endpoint = ["http://${data.kubernetes_service.nexus_docker_lb.status[0].load_balancer[0].ingress[0].hostname}:8082"]' /etc/containerd/config.toml
+
+                systemctl enable containerd
+                systemctl restart containerd
+
+                # Bootstrap EKS
+                /etc/eks/bootstrap.sh ${local.name}
+                EOT
+    )
 
   tag_specifications {
     resource_type = "instance"
